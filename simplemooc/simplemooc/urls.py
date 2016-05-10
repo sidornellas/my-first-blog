4d4e047817e563fc5a83c18474f085f17b4c9c5d
sidornellas <sidornellas@gmail.com>
2016-05-09 21:14:27 -0300

    curso django - section 1 - my first commit

--- simplemooc/simplemooc/urls.py
+++ simplemooc/simplemooc/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""simplemooc URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
@@ -18,6 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('blog.urls')),
-    url(r'', include('simplemooc.urls')),
+    url(r'^', include('simplemooc.core.urls', namespace='core')),
 ]
